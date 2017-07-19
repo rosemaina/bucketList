dad214948228a42ba083699b23237d93b6df5220
rosemaina <rosemaina32@gmail.com>
2017-07-19 14:32:32 +0300

    Remove user_id

--- app/models/bucketlist.py
+++ app/models/bucketlist.py
@@ -4,18 +4,17 @@ from app.models.data import Data
 class Bucketlist(object):
     """This is the main class"""
 
-    def __init__(self, title, intro, user_id, _id=None):
+    def __init__(self, title, intro, _id):
         self.title = title
         self.intro = intro
-        self.user_id = user_id
         self._id = _id
 
-    def create_item(self, item_name, intro, bucketlist_id=None):
+    def create_item(self, item_name, intro, bucketlist_id):
         """method used for creating an item"""
         item = Item(
             item_name=item_name,
             intro=intro,
-            bucketlist_id=self._id
+            bucketlist_id=bucketlist_id
         )
         # this saves items
         item.save_into_item()
@@ -25,7 +24,6 @@ class Bucketlist(object):
         return {
             'title': self.title,
             'intro': self.intro,
-            'user_id': self.user_id,
             '_id': self._id
             }
 
