2956edbeff66e78940a8612de2361614dd6c502f
rosemaina <rosemaina32@gmail.com>
2017-07-20 12:38:18 +0300

    Add flask routes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,37 @@
+"""This script Creates the application object"""
 from app import app
+from app.models.user import User
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['POST', 'GET'])
 def index():
-    return "'
+    """renders the homepage of the app"""
+    #this will get you the variables when someone submits
+    #the form since we have set our forms method to POST
+    data = {'host_url': request.host_url}
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    return render_template('index.html')
+
+
+@app.route('/registration')
+def registration():
+    """renders the registration page of the app"""
+    user = {
+        "email": "jdoe@email",
+        "password": "4444",
+        "confirm_password": "4444"
+        }
+
+    return render_template('registration.html', user=user)
+
+
+@app.route('/create_list')
+def create_list():
+    """Renders the create bucketlist of the app"""
+    return render_template('create_list.html')
