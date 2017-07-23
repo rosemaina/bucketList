2fe5d3c7d15d2de6784995713c137f20ae73a4f9
rosemaina <rosemaina32@gmail.com>
2017-07-23 14:22:33 +0300

    Add tests for update function

--- app/models/tests/bucketlist_tests.py
+++ app/models/tests/bucketlist_tests.py
@@ -8,10 +8,14 @@ class Testbucketlist(unittest.TestCase):
     """Bucketlist test class"""
     def setUp(self):
         self.data = Data()
-        self.item1 = {'bucketlist_id': '',
-                      'item_name': 'skinny dipping',
-                      'intro': 'will jump at Sagana-River Tana'
-                      }
+        self.bucket101 = Bucketlist('bronches bucketlist',
+                                    'summer activities', '1010')
+        self.bucketlist1 = {'title': 'bucket list 101',
+                            'intro': 'my first bucketlist',
+                            '_id': 12345}
+        self.item1 = {'item_name': 'skinny dipping',
+                      'intro': 'will jump at Sagana-River Tana',
+                      'bucketlist_id': ''}
 
     def test_create_item(self):
         """Tests if a new  item has been created"""
@@ -19,6 +23,14 @@ class Testbucketlist(unittest.TestCase):
         bucket.create_item('skinny dipping', 'item introduction', '4444')
         result = len(self.data.all_items)
         self.assertEqual(result, 1)
+
+    def test_update_bucket_item(self):
+        """Tests if a bucketlist item can be updated"""
+        self.data.all_items.append(self.item1)
+        result = self.bucket101.update_bucket_item('learn python',
+                                                   'study tdd', '1010')
+        self.assertEqual(result, 'Item successfully updated')
+
 
 if __name__ == '__main__':
     unittest.main()
