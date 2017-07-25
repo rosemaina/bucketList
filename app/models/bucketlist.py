8ab5a337570fbf88b8e064931eaccc8503eea2f0
rosemaina <rosemaina32@gmail.com>
2017-07-26 00:07:34 +0300

    Remove functionalities

--- app/models/bucketlist.py
+++ app/models/bucketlist.py
@@ -1,45 +1,47 @@
 """ This is the main module for the bucketlist """
-from app.models.item import Item
-from app.models.data import Data
-
+import uuid
 
 class Bucketlist(object):
     """This is the main class"""
 
-    def __init__(self, title, intro, _id):
+    def __init__(self, title, intro, _id=None):
         self.title = title
         self.intro = intro
-        self._id = _id
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def create_item(self, item_name, intro, bucketlist_id):
-        """method used for creating an item"""
-        item = Item(
-            item_name=item_name,
-            intro=intro,
-            bucketlist_id=bucketlist_id
-        )
-        # this saves items
-        item.save_into_item()
+    # def create_bucketlist(self):
+    #     """Method used for creating a bucketlist"""
+    #     # creates an object of bucketlist and saves it
+    #     self.all_bucketlists[self._id] = {'titleself.title}
+    # # def create_item(self, item_name, intro, bucketlist_id):
+    #     """method used for creating an item"""
+    #     item = Item(
+    #         item_name=item_name,
+    #         intro=intro,
+    #         bucketlist_id=bucketlist_id
+    #     )
+    #     # this saves items
+    #     item.save_into_item()
 
-    def update_bucket_item(self, item_name, intro, bucketlist_id):
-        """Method used to update a bucketlist item"""
-        my_item = Data.all_items
-        for num in range(0, len(my_item)):
-            if bucketlist_id == my_item[num]['bucketlist_id']:
-                # gives the bucketlist index and keys whose value are updated
-                my_item[num]['item_name'] = item_name
-                my_item[num]['intro'] = intro
-                break
-        return "Item successfully updated"
+    # def update_bucket_item(self, item_name, intro, bucketlist_id):
+    #     """Method used to update a bucketlist item"""
+    #     my_item = Data.all_items
+    #     for num in range(0, len(my_item)):
+    #         if bucketlist_id == my_item[num]['bucketlist_id']:
+    #             # gives the bucketlist index and keys whose value are updated
+    #             my_item[num]['item_name'] = item_name
+    #             my_item[num]['intro'] = intro
+    #             break
+    #     return "Item successfully updated"
 
-    def bucketlist_data(self):
-        """Returns data to be saved in the all_bucketlist list"""
-        return {
-            '_id': self._id,
-            'title': self.title,
-            'intro': self.intro
-            }
+    # def bucketlist_data(self):
+    #     """Returns data to be saved in the all_bucketlist list"""
+    #     return {
+    #         '_id': self._id,
+    #         'title': self.title,
+    #         'intro': self.intro
+    #         }
 
-    def save_into_bucketlist(self):
-        """Method saves into bucketlist """
-        Data.all_bucketlists.append(self.bucketlist_data())
+    # def save_into_bucketlist(self):
+    #     """Method saves into bucketlist """
+    #     Data.all_bucketlists.append(self.bucketlist_data())
