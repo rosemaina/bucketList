640c19856b84fb79984593cf707430c7cdf0634b
rosemaina <rosemaina32@gmail.com>
2017-07-27 13:24:01 +0300

    Delete config file

--- application/models/user.py
+++
@@ -1,9 +0,0 @@
-"""This is the user module"""
-
-
-class User(object):
-    """The main user class"""
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
--- config.py
+++
@@ -1,6 +0,0 @@
-"""Main config class"""
-
-
-class Config(object):
-    """Parent configuration class."""
-    DEBUG = False
